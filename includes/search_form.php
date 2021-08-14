177621603a3718e1cc87b6b4471a5f0ae61cbe8e
naveen-ku <naveen2951996@gmail.com>
2021-08-14 13:01:17 +0530

    [feat] add search engine functionality
    Search engine searches based on post_tags of posts

---
+++ includes/search_form.php
@@ -0,0 +1,14 @@
+<div class="well">
+    <h4>Blog Search</h4>
+    <form action="search.php" method="POST">
+        <div class="input-group">
+            <input name="search" type="text" class="form-control">
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="submit" name="submit">
+                    <span class="glyphicon glyphicon-search"></span>
+                </button>
+            </span>
+        </div>
+    </form>
+
+</div>
